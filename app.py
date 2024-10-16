376f913906fa7039471b7a6cddd744aeb71dbde0
Carlos Ortiz <carlosortizbon@gmail.com>
2024-10-16 11:33:40 -0500

    change impuesto específico

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ with tab1:
 
     rec_int = df[df['Agregación'] == 'Actividad interna']
     piv_recaudo = (rec_int
-                                .groupby(['Año', 'Impuesto_alt'])['Valor (Constantes - 18)']
+                                .groupby(['Año', 'Impuesto específico'])['Valor (Constantes - 18)']
                                 .sum()
                                 .reset_index())
     acum = (piv_recaudo
@@ -104,7 +104,7 @@ with tab1:
                 'Renta':DIC_COLORES['ax_viol'][1],
                 'Otros':DIC_COLORES['ro_am_na'][3]}
 
-    for i, group in piv_recaudo.groupby('Impuesto_alt'):
+    for i, group in piv_recaudo.groupby('Impuesto específico'):
                     fig.add_trace(go.Bar(
                         x=group['Año'],
                         y=group['%'],
